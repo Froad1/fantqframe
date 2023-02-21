4efe25ee9b7d65551be6bcf7af82aa39fa58ee5e
Dmytro Savchuk <xsavchuk@fi.muni.cz>
2023-02-21 19:32:28 +0100

    creating a base

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const gallery = document.querySelector(".images");
+
+fetch("img/")
+  .then(response => response.text())
+  .then(text => {
+    const files = text.split("\n");
+    const imageFiles = files.filter(file => file.match(/\.(jpeg|jpg|png|gif)$/));
+    console.log(imageFiles)
+    imageFiles.forEach(imageFile => {
+      const img = document.createElement("img");
+      img.src = "img/" + imageFile;
+      gallery.appendChild(img);
+    });
+  });
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', async (event) => {
+event.preventDefault();
+const formData = new FormData(form);
+const response = await fetch('/upload', {
+    method: 'POST',
+    body: formData,
+});
+const result = await response.json();
+console.log(result);
+});
