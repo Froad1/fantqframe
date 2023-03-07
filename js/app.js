9deec6ceb1a71149e2c6ca848e49a9292d4a73f7
Dmytro Savchuk <xsavchuk@fi.muni.cz>
2023-03-07 02:02:05 +0100

    beta 1.1.1 add detailed info

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       for (var i = 0; i < items.length; i++) {
           var item = items[i];
           if (item.mimeType.indexOf('image') !== -1) {
-			$('.image_container').append('<div class="images"><img src="' + item.thumbnailLink.split('=')[0] + '" class="image"><div class="image-info"><span class="material-symbols-outlined download" data-image-src="' + item.thumbnailLink.split('=')[0] + '" data-image-name="' + item.name + '" >download</span><a class="material-symbols-outlined max-size" href="' + item.thumbnailLink.split('=')[0] + '" target="_blank">open_in_full</a></div></div>')
+			$('.image_container').append('<div class="images"><img src="' + item.thumbnailLink.split('=')[0] + '" class="image"><div class="image-info"><span class="material-symbols-outlined download" data-image-src="' + item.thumbnailLink.split('=')[0] + '" data-image-name="' + item.name + '" >download</span><a class="material-symbols-outlined max-size" href="' + item.thumbnailLink.split('=')[0] + '" target="_blank">open_in_full</a><span class="material-symbols-outlined more_horiz" data-img-metadans="' + i + '">more_horiz</span></div></div>')
 			}
 		}
 		$('.image').ready(function(){
@@ -66,7 +66,43 @@ $(document).ready(function() {
 				  }
 				  
 			});
-		})
+
+			$('.more_horiz').on('click',function(){
+				$('.detail-image-info').html('');
+				$('.detail-image-info').show(500, function(){
+					$('.detail-image-info').css("display", "flex");
+				})
+				$('.detail-image-info').append('<div class="detail-info"></div>')
+				var metadans = $(this).data('img-metadans');
+				console.log(items[metadans].imageMediaMetadata);
+				if(items[metadans].imageMediaMetadata.cameraModel == "Canon EOS 1300D"){
+					$('.detail-info').append('<img src="./assets/A034.png" class="detail-img"><div class="camera-info"><p class="camera-model">Камера: '+ items[metadans].imageMediaMetadata.cameraModel +'</p><p>Обʼєктив: '+ items[metadans].imageMediaMetadata.lens +'</p></div>')
+				}
+				else if(items[metadans].imageMediaMetadata.cameraModel == "Pixel 4 XL"){
+					$('.detail-info').append('<img src="./assets/pixel4.jpeg" class="detail-img"><div class="camera-info"><p class="camera-model">Камера: '+ items[metadans].imageMediaMetadata.cameraModel +'</p><p>Обʼєктив: '+ items[metadans].imageMediaMetadata.lens +'</p></div>')
+				}
+				else $('.detail-info').append('<p>Невідомо</p>')
+			});
+
+			$('.detail-image-info').on('click', function(){
+				closeDet();
+			});
+			$(document).keydown(function(event) {
+				if (event.keyCode === 27) { // 27 - код клавіші "Esc"
+				  closeDet();
+				}
+			  });
+			$('.big-close').on('click', function(){
+				closeDet();
+			});
+
+
+			function closeDet(){
+				$('.detail-image-info').hide(500, function(){
+					$('.detail-image-info').html('');
+				})
+			}
+		});
 	});
 });
 
