18ae2b168b8577fb096ba9af78c57b2713ac63e4
Dmytro Savchuk <xsavchuk@fi.muni.cz>
2023-03-19 22:27:52 +0100

    beta 1.2 add photo location, add shadow for image box

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
             '&key=' + apiKey;
   $.getJSON(url, function(data) {
       var items = data.files;
+	  console.log(items);
       for (var i = 0; i < items.length; i++) {
           var item = items[i];
           if (item.mimeType.indexOf('image') !== -1) {
@@ -24,8 +25,10 @@ $(document).ready(function() {
 				$('.big-image_container').append('<div class="big-background"> <img src="'+ imageSrc +'" class="big-image"> <span class="material-symbols-outlined big-close">close</span></div>')
 			})
 
-			$('.big-image_container').on('click', function(){
-				closeBig();
+			$('.big-image_container').on('click', function(e){
+				if(e.target === document.querySelector(".big-image_container")){
+					closeBig();
+				}
 			});
 			$(document).keydown(function(event) {
 				if (event.keyCode === 27) { // 27 - код клавіші "Esc"
@@ -72,16 +75,24 @@ $(document).ready(function() {
 				$('.detail-image-info').show(500, function(){
 					$('.detail-image-info').css("display", "flex");
 				})
-				$('.detail-image-info').append('<div class="detail-info"></div>')
+
 				var metadans = $(this).data('img-metadans');
 				console.log(items[metadans].imageMediaMetadata);
+				var camera 
 				if(items[metadans].imageMediaMetadata.cameraModel == "Canon EOS 1300D"){
-					$('.detail-info').append('<img src="./assets/A034.png" class="detail-img"><div class="camera-info"><p class="camera-model">Камера: '+ items[metadans].imageMediaMetadata.cameraModel +'</p><p>Обʼєктив: '+ items[metadans].imageMediaMetadata.lens +'</p></div>')
+					camera = '<img src="./assets/A034.png" class="detail-img"><div class="camera-info"><p class="camera-model">Камера: '+ items[metadans].imageMediaMetadata.cameraModel +'</p><p>Обʼєктив: '+ items[metadans].imageMediaMetadata.lens +'</p></div>'
 				}
 				else if(items[metadans].imageMediaMetadata.cameraModel == "Pixel 4 XL"){
-					$('.detail-info').append('<img src="./assets/pixel4.jpeg" class="detail-img"><div class="camera-info"><p class="camera-model">Камера: '+ items[metadans].imageMediaMetadata.cameraModel +'</p><p>Обʼєктив: '+ items[metadans].imageMediaMetadata.lens +'</p></div>')
+					camera = '<img src="./assets/pixel4.jpeg" class="detail-img"><div class="camera-info"><p class="camera-model">Камера: '+ items[metadans].imageMediaMetadata.cameraModel +'</p></div>'
 				}
-				else $('.detail-info').append('<p>Невідомо</p>')
+				else{
+					camera = '<p>Невідомо</p>'
+				} 
+
+				$('.detail-image-info').append('<div class="detail-info">' +camera+'</div>')
+				items[metadans].imageMediaMetadata.location.latitude ? $('.camera-info').append('<p>Місце зйомки:</p><img src="https://maps.googleapis.com/maps/api/staticmap?center='+ items[metadans].imageMediaMetadata.location.latitude +',' + items[metadans].imageMediaMetadata.location.longitude +'&zoom=12&size=300x300&markers=color:red%7C48.14255555555555,17.10073888888889&maptype=roadmap&key=AIzaSyCSb_Cp8WHrUORs07iAZy4S4DruUsFHPw0&map_id=4be591a3da52b40f" alt="" srcset="" class="map">') :"";
+					//AIzaSyCSb_Cp8WHrUORs07iAZy4S4DruUsFHPw0
+				
 			});
 
 			$('.detail-image-info').on('click', function(){
@@ -104,5 +115,14 @@ $(document).ready(function() {
 			}
 		});
 	});
+	$('.small-wrap').on('click',function(){
+		$('.image').css('max-height', '35vh')
+	})
+	$('.big-wrap').on('click',function(){
+		$('.image').css('max-height', '60vh')
+	})
 });
+
+
+
 
